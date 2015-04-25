a607315f317d6511d95f9ce3ecc875d766910494
Takashi Oshikawa <abr3tks5200@ezweb.ne.jp>
2015-04-26 08:50:47 +0900

    データをJSON形式で返す

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,11 +2,24 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import play.api.libs.json._
+import play.api.libs.json
 
 object Application extends Controller {
 
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+
+      //データをJsonで返す
+      val member = Json.toJson(
+         List(
+           Map("name" -> Json.toJson("Gerrard"),   "number" -> Json.toJson(8),  "age" -> Json.toJson(34)),
+           Map("name" -> Json.toJson("Henderson"), "number" -> Json.toJson(14), "age" -> Json.toJson(25)),
+           Map("name" -> Json.toJson("Skrtel"),    "number" -> Json.toJson(37), "age" -> Json.toJson(31))
+         )
+      )
+
+    Ok( member ).withHeaders("Access-Control-Allow-Origin" -> " *")
+//    Ok(views.html.index("Yes!!"))
   }
 
 }
